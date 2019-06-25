55a06dc27aafa3d102d4f95d41aae5745277120f
Raghav Maheshwari <raghav.ddps2@gmail.com>
2019-06-25 14:17:32 +0530

    Adding solutions for todays problems

--- src/Lecture5/uniqueArrayElement.java
+++ src/Lecture5/uniqueArrayElement.java
@@ -2,13 +2,7 @@ public class uniqueArrayElement{
 
 	public static int findUnique(int[] arr){
 
-        /* Your class should be named FindUnique
-		 * Don't write main().
-		 * Don't read input, it is passed as function argument.
-		 * Return output and don't print it.
-	 	 * Taking input and printing output is handled automatically.
-		 */
-        //Basically important thing is that we are keeping a count of the frequency of elements
+        
         int res,k =0;
         int max = arr[0];
         for(int i=0;i<arr.length;i++){

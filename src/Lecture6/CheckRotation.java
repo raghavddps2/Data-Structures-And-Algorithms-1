46b984560d493e92f7fdda2101f2131883a825d1
Raghav Maheshwari <raghav.ddps2@gmail.com>
2019-06-29 12:49:12 +0530

    Better way to solve CheckArrayIndex

--- src/Lecture6/CheckRotation.java
+++ src/Lecture6/CheckRotation.java
@@ -18,26 +18,34 @@ public class CheckRotation {
 	public static int arrayRotateCheck(int[] arr){
 		
             
-        int n = arr.length;
-        int arr2[] = new int[n];
-        //assigning values to the new array.
-        for(int i=0;i<n;i++){
-            arr2[i] = arr[i];
-        }
-        //sorting the original array.
-        Arrays.sort(arr);
+        // int n = arr.length;
+        // int arr2[] = new int[n];
+        // //assigning values to the new array.
+        // for(int i=0;i<n;i++){
+        //     arr2[i] = arr[i];
+        // }
+        // //sorting the original array.
+        // Arrays.sort(arr);
         
-        //starting the loop to check the rotation index(how many times rotation is done.)
-        for(int i=1;i<n;i++){
+        // //starting the loop to check the rotation index(how many times rotation is done.)
+        // for(int i=1;i<n;i++){
             
-            //rotating
-            rotateClockwise(arr,1);
-            //comparing here.
-            boolean cmp = Arrays.equals(arr2,arr);
+        //     //rotating
+        //     rotateClockwise(arr,1);
+        //     //comparing here.
+        //     boolean cmp = Arrays.equals(arr2,arr);
             
-            if(cmp){
-                //if matches,return;
-                return i;
+        //     if(cmp){
+        //         //if matches,return;
+        //         return i;
+        //     }
+        // }
+        // return 0;
+
+        int n = arr.length;
+        for(int i=0;i<n-1;i++){
+            if(arr[i]>arr[i+1]){
+                return (i+1);
             }
         }
         return 0;

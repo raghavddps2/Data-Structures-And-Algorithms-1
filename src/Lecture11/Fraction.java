1385da39fa3cb70d26606ad76911175706775b4e
Raghav Maheshwari <raghav.ddps2@gmail.com>
2019-07-16 14:05:20 +0530

    Adding code for adding two fractions

--- src/Lecture11/Fraction.java
+++ src/Lecture11/Fraction.java
@@ -2,6 +2,7 @@ package Lecture11;
 
 /**
  * Fraction
+ * No need of lcm and all. All that is unnecessary.
  */
 public class Fraction {
 
@@ -21,25 +22,25 @@ public class Fraction {
     // }
 
     //Here is the iterative hcf.
-    public int hcf(int a,int b){
-        while(a!=b){
-            if(a>b){
-                a = a-b;
-            }
-            else{
-                b = b-a;
-            }
-        }
-        return a;
-    }
-    public int lcm(int a,int b){
-        int hcfRes = hcf(a,b);
-        int lcmRes = (a*b)/hcfRes;
-        return lcmRes;
-    }
+    // public int hcf(int a,int b){
+    //     while(a!=b){
+    //         if(a>b){
+    //             a = a-b;
+    //         }
+    //         else{
+    //             b = b-a;
+    //         }
+    //     }
+    //     return a;
+    // }
+    // public int lcm(int a,int b){
+    //     int hcfRes = hcf(a,b);
+    //     int lcmRes = (a*b)/hcfRes;
+    //     return lcmRes;
+    // }
     public Fraction add(Fraction f1,Fraction f2){
         Fraction f3 = new Fraction(0,0);
-        f3.denominator = lcm(f1.denominator,f2.denominator);
+        f3.denominator = f1.denominator*f2.denominator;
         f3.numerator = (f1.numerator*f2.denominator)+(f2.numerator*f1.denominator);
         return f3;
     }
